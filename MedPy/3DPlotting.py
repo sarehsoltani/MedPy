3578c49409ebbd429531febf28b6fc3ae695a21b
Sare Soltani <sare.soltani96@yahoo.com>
2020-09-03 22:47:18 +0430

    Hounsfield Units

--- MedPy/3DPlotting.py
+++ MedPy/3DPlotting.py
@@ -27,19 +27,45 @@ def window_image(image, window_center, window_width):
     return window_image
 
 def load_and_plot_image(file_path, save=False):
-    medical_image = pydicom.read_file(file_path)
-    image = medical_image.pixel_array
-    print(medical_image)
-    print(image.shape)
-    
-    print(image.mode)
-    print(image.dtype)
-    print(image.shape)
+    #medical_image = pydicom.read_file(file_path)
+    #image = medical_image.pixel_array
+   #data_set = dicom.read_file(path)
+    #pixel_array = data_set.pixel_array
+    #size_of_array = image.shape
+    ct_dicom = pydicom.read_file(file_path)   
+    img = ct_dicom.pixel_array
+    img_2d = img.astype(float)
+
+## Step 2. Rescaling grey scale between 0-255
+    img_2d_scaled = (np.maximum(img_2d,0) / img_2d.max()) * 255.0
+
+## Step 3. Convert to uint
+    img_2d_scaled = np.uint8(img_2d_scaled)
+
+
+# Show information of input and output in above code
+## (1) Show information of original CT image 
+    print(img.dtype)
+    print(img.shape)
+    print(img)
+
+## (2) Show information of gray image of it 
+    print(img_2d_scaled.dtype)
+    print(img_2d_scaled.shape)
+    print(img_2d_scaled)
+
+## (3) Show the scaled gray image by matplotlib
+    plt.imshow(img_2d_scaled, cmap='gray', vmin=0, vmax=255)
+    plt.show()
+    #print(chanR)
+    #print(image)
+    #print(image.dtype)
+    #print(image.shape)
   
     
-    hu_image = transform_to_hu(medical_image, image)
-    brain_image = window_image(hu_image, 40, 80)
-    bone_image = window_image(hu_image, 400, 1000)
+    #hu_image = transform_to_hu(medical_image, image)
+    #brain_image = window_image(hu_image, 40, 80)
+    #bone_image = window_image(hu_image, 400, 1000)
     
     #plt.figure(figsize=(20, 10))
     #plt.style.use('grayscale')
